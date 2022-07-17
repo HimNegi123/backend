d496baa9fbed7e8f6dbc11976341d3850e437a89
Himanshu <hnegi8984@gmail.com>
2022-07-17 22:44:10 +0530

    database Ready to use

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app=express();
 app.use(express.json());
 app.get('',async(req,res)=>{
     let data=await product.find();
+    console.log(data);
     res.send(data);
 });
 app.post('',async(req,res)=>{
@@ -13,4 +14,10 @@ app.post('',async(req,res)=>{
     await data.save();
 res.send('hellow ');
 });
+
+app.delete('/:_id',async(req,res)=>{
+console.log(req.params);
+    let data=await product.deleteOne(req.params)
+res.send(data);
+})
 app.listen(process.env.PORT||5000);
